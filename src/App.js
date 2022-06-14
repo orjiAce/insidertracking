3d1e7e8097f21081e6ed7c24434474fd910e1bea
orjiace <orjiace@gmail.com>
2022-06-14 09:52:34 +0100

    added FB index persistence

--- src/App.js
+++ src/App.js
@@ -12,7 +12,21 @@ import {persistor, store} from "./app/store";
 import {QueryClientProvider, QueryClient} from 'react-query'
 
 // ----------------------------------------------------------------------
+import { enableIndexedDbPersistence } from "firebase/firestore";
+import {db} from "./firebase";
 
+enableIndexedDbPersistence(db)
+    .catch((err) => {
+        if (err.code == 'failed-precondition') {
+            // Multiple tabs open, persistence can only be enabled
+            // in one tab at a a time.
+            // ...
+        } else if (err.code == 'unimplemented') {
+            // The current browser does not support all of the
+            // features required to enable persistence
+            // ...
+        }
+    });
 export default function App() {
     const queryClient = new QueryClient()
   return (
