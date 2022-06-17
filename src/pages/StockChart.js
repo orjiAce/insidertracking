60f2d3bad69192ed193b54d20d20d0e5f49fc18e
orjiace <orjiace@gmail.com>
2022-06-17 20:55:45 +0100

    adding chart

---
+++ src/pages/StockChart.js
@@ -0,0 +1,144 @@
+import { sentenceCase } from 'change-case';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+// @mui
+import { alpha, styled } from '@mui/material/styles';
+import { Box, Tab, Card, Grid, Divider, Container, Typography } from '@mui/material';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+// redux
+
+
+
+
+// components
+import Page from "../components/Page";
+import Iconify from '../components/Iconify';
+import Markdown from '../components/Markdown';
+import { SkeletonProduct } from '../components/skeleton';
+
+// sections
+import {
+  ProductDetailsSummary,
+  ProductDetailsReview,
+  ProductDetailsCarousel,
+} from '../sections/@dashboard/e-commerce/product-details';
+
+import {useDispatch, useSelector} from "react-redux";
+
+// ----------------------------------------------------------------------
+
+const PRODUCT_DESCRIPTION = [
+  {
+    title: '100% Original',
+    description: 'Chocolate bar candy canes ice cream toffee cookie halvah.',
+    icon: 'ic:round-verified',
+  },
+  {
+    title: '10 Day Replacement',
+    description: 'Marshmallow biscuit donut dragée fruitcake wafer.',
+    icon: 'eva:clock-fill',
+  },
+  {
+    title: 'Year Warranty',
+    description: 'Cotton candy gingerbread cake I love sugar sweet.',
+    icon: 'ic:round-verified-user',
+  },
+];
+
+const IconWrapperStyle = styled('div')(({ theme }) => ({
+  margin: 'auto',
+  display: 'flex',
+  borderRadius: '50%',
+  alignItems: 'center',
+  width: theme.spacing(8),
+  justifyContent: 'center',
+  height: theme.spacing(8),
+  marginBottom: theme.spacing(3),
+  color: theme.palette.primary.main,
+  backgroundColor: `${alpha(theme.palette.primary.main, 0.08)}`,
+}));
+
+// ----------------------------------------------------------------------
+
+export default function EcommerceProductDetails() {
+
+  const dispatch = useDispatch();
+  const [value, setValue] = useState('1');
+  const { name = '' } = useParams();
+  const { product, error, checkout } = useSelector((state) => state.product);
+
+
+
+  return (
+    <Page title="Ecommerce: Product Details">
+      <Container maxWidth="xl">
+
+
+
+
+        {product && (
+          <>
+            <Card>
+              <Grid container>
+                <Grid item xs={12} md={6} lg={7}>
+       <Typography> CHART AND THINGS</Typography>
+
+                </Grid>
+                <Grid item xs={12} md={6} lg={5}>
+                  <Typography> FOOTER CHART AND THINGS FOOTER </Typography>
+                </Grid>
+              </Grid>
+            </Card>
+
+            <Grid container sx={{ my: 8 }}>
+              {PRODUCT_DESCRIPTION.map((item) => (
+                <Grid item xs={12} md={4} key={item.title}>
+                  <Box sx={{ my: 2, mx: 'auto', maxWidth: 280, textAlign: 'center' }}>
+                    <IconWrapperStyle>
+                      <Iconify icon={item.icon} width={36} height={36} />
+                    </IconWrapperStyle>
+                    <Typography variant="subtitle1" gutterBottom>
+                      {item.title}
+                    </Typography>
+                    <Typography sx={{ color: 'text.secondary' }}>{item.description}</Typography>
+                  </Box>
+                </Grid>
+              ))}
+            </Grid>
+
+            <Card>
+              <TabContext value={value}>
+                <Box sx={{ px: 3, bgcolor: 'background.neutral' }}>
+                  <TabList onChange={(e, value) => setValue(value)}>
+                    <Tab disableRipple value="1" label="Description" />
+                    <Tab
+                      disableRipple
+                      value="2"
+                      label={`Review (${product.reviews.length})`}
+                      sx={{ '& .MuiTab-wrapper': { whiteSpace: 'nowrap' } }}
+                    />
+                  </TabList>
+                </Box>
+
+                <Divider />
+
+                <TabPanel value="1">
+                  <Box sx={{ p: 3 }}>
+                    <Markdown children={product.description} />
+                  </Box>
+                </TabPanel>
+                <TabPanel value="2">
+                  <ProductDetailsReview product={product} />
+                </TabPanel>
+              </TabContext>
+            </Card>
+          </>
+        )}
+
+        {!product && <SkeletonProduct />}
+
+        {error && <Typography variant="h6">404 Product not found</Typography>}
+      </Container>
+    </Page>
+  );
+}
